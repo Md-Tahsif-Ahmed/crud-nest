b16785b8e94482ee53ea58b5753bafa1c8dcdbd9
Md-Tahsif-Ahmed <tahsif.cse@gmail.com>
2025-04-29 13:44:34 +0600

    postcategory multi category create refresh

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -7,6 +7,7 @@ import { Post } from './entities/post.entity';
 import { NotFoundException } from '@nestjs/common';
 import { Category } from 'src/category/entities/category.entity';
 import { PostCategory } from 'src/category/entities/post-category.entity';
+import { QueryRunner, DataSource } from 'typeorm';
 
 @Injectable()
 export class PostService {
@@ -17,29 +18,45 @@ export class PostService {
     private readonly postCategoryRepository: Repository<PostCategory>,
     @InjectRepository(Category)
     private readonly categoryRepository: Repository<Category>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async createPost(createPostDto: CreatePostDto): Promise<Post> {
     const { title, content, categories } = createPostDto;
-    const post = this.postRepository.create({ title, content });
-    const savedPost = await this.postRepository.save(post);
-    if (!savedPost) {
-      throw new NotFoundException('Post not created');
-    }
-    for (const categoryItem of categories) {
-      const category = await this.categoryRepository.findOne({
-        where: { id: categoryItem.categoryId },
-      });
-      if (category) {
-        const postCategory = new PostCategory();
-        postCategory.post = savedPost;
-        postCategory.category = category;
-        postCategory.note = categoryItem.note;
-        await this.postCategoryRepository.save(postCategory);
-      }
-    }
 
-    return savedPost;
+    const queryRunner: QueryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.startTransaction();
+
+    try {
+      const post = this.postRepository.create({ title, content });
+      const savedPost = await queryRunner.manager.save(post);
+      const postCategories: PostCategory[] = [];
+      for (const categoryItem of categories) {
+        const category = await this.categoryRepository.findOne({
+          where: { id: categoryItem.categoryId },
+        });
+        if (category) {
+          const postCategory = new PostCategory();
+          postCategory.post = savedPost;
+          postCategory.category = category;
+          postCategory.note = categoryItem.note;
+          postCategories.push(postCategory);
+        }
+      }
+
+      if (postCategories.length > 0) {
+        await queryRunner.manager.save(PostCategory, postCategories);
+      }
+
+      await queryRunner.commitTransaction();
+
+      return savedPost;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async updatePost(id: number, updatePostDto: UpdatePostDto): Promise<Post> {
